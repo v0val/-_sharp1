b6604ab9366812dba43c1c841090848ae4a810b9
valeryO <olennval@mail.ru>
2022-08-03 23:56:28 +0700

    if_else_privetstvie

---
+++ Examples/Example005_Uslovya/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите Ваше имя");
+string nameUser = Console.ReadLine();
+if (nameUser.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же Маша!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(nameUser);
+}
